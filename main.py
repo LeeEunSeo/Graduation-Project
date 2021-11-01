8e14f68db8834d8fd322c95a01dc64fffcb3b9c6
Numonov Sardorbek <numonovsardorbek@gmail.com>
2021-11-01 22:47:01 +0500

    done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from main_function import ssd_rtdb
 
 app = Flask(__name__)
@@ -9,3 +9,14 @@ def index():
     user_id = "user2"
     ssd_rtdb(user_id)
     return "<h1>Welcome to CodingX</h1>"
+
+
+@app.route('/rec_choice/<user_id>', methods=['POST'])
+def generate(user_id):
+    if request.method == 'POST':
+        if user_id:
+            ssd_rtdb(user_id)
+            return "<h1>Success</h1>"
+        else:
+            return "<h1>Failed</h1>"
+    return "<h1>Incorrect</h1>"
